9eab54095dc7b6a388b31dc8d050fed34c5eed53
tritiatimmins <tritialtimmins@gmail.com>
2016-02-10 19:38:47 -0800

    my tunes progress

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,20 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
+  
+  tagName: "table"
 
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+          return new SongQueueView({model: song}).render();
+      })
+    );
   }
 
 });
